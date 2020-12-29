01db9f56d697b871f2f1e8ade805738598bf3640
mrogovets <34492317+mrogovets@users.noreply.github.com>
2020-12-29 23:09:16 +0200

    fs

---
+++ demo/fs.js
@@ -0,0 +1,3 @@
+const fs = require("fs"); // file system
+
+fs.mkdir;
--- demo/path.js
+++ demo/path.js
@@ -1,1 +1,11 @@
 const path = require("path");
+
+console.log("FileName: ", path.basename(__filename));
+
+console.log("Directory: ", path.dirname(__filename));
+
+console.log("extensions: ", path.extname(__filename));
+
+console.log("Parse", path.parse(__filename));
+
+console.log(path.join(__dirname, "server", "index.html"));
