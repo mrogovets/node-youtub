a3a7fb3bdf6470c88f6121e2b962c3c998f8e9d5
mrogovets <34492317+mrogovets@users.noreply.github.com>
2021-01-03 19:47:06 +0200

    fs module

--- demo/fs.js
+++ demo/fs.js
@@ -1,3 +1,34 @@
 const fs = require("fs"); // file system
+const path = require("path");
 
-fs.mkdir; // 22:26
+// fs.mkdir(path.join(__dirname, "test"), (err) => {
+//   if (err) {
+//     throw err;
+//   }
+//   console.log("Directory is made");
+// });
+
+const filePath = path.join(__dirname, "test", "text.txt");
+
+// fs.writeFile(filePath, "Hello World!", (err) => {
+//   if (err) {
+//     throw err;
+//   }
+//   console.log("File is made");
+// });
+
+// fs.appendFile(filePath, "\nHello World, again!", (err) => {
+//   if (err) {
+//     throw err;
+//   }
+//   console.log("File is made");
+// });
+
+fs.readFile(filePath, "utf-8", (err, content) => {
+  if (err) {
+    throw err;
+  }
+  console.log(content);
+  //   const data = Buffer.from(content);
+  //   console.log("Content: ", data.toString());
+});
